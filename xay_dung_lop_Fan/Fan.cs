b335c3e73d6f78c4e72f0b16abfb97bcbfd8a1f8
unknown <leconghoangphuc@gmail.com>
2020-12-17 19:16:06 +0700

    fix

--- xay_dung_lop_Fan/Fan.cs
+++ xay_dung_lop_Fan/Fan.cs
@@ -1,22 +1,43 @@
-namespace xay_dung_lop_Fan
-{
-    public class Fan
-    {
-        private const int _fast = 3;
-       
-        private const int _slow = 1;
-     
-        private const int _medium = 2;
-      private int myVar;
-      public int MyProperty
-      {
-          get { return myVar; }
-          set { myVar = value; }
-      }
-      
+using System;
+using System.Collections.Generic;
+using System.Text;
 
-        
-        
-        
+namespace XAY_DUNG_LOP_FAN
+{
+    class Fan
+    {
+        private const byte _SLOW = 1;
+        private const byte _MEDIUM = 2;
+        private const byte _FAST = 3;
+        private int _speed = SLOW;
+        private bool _on = false;
+        private double _radius = 5;
+        private string _color = "blue";
+
+        public static byte SLOW => _SLOW;
+        public static byte MEDIUM => _MEDIUM;
+        public static byte FAST => _FAST;
+        public int Speed { get => _speed; set => _speed = value; }
+        public bool On { get => _on; set => _on = value; }
+        public double Radius { get => _radius; set => _radius = value; }
+        public string Color { get => _color; set => _color = value; }
+
+
+
+        public Fan()
+        {
+
+        }
+
+        public string DisplayFan()
+        {
+            if (On)
+            {
+                return $"Tốc độ quạt {Speed}, màu quạt {Color}, bán kính quạt {Radius} và quạt đang bật";
+            } else
+            {
+                return $"Màu quạt {Color}, bán kính quạt {Radius} và quạt đang tắt";
+            }
+        }
     }
 }
--- xay_dung_lop_Fan/Program.cs
+++ xay_dung_lop_Fan/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Text;
 
-namespace xay_dung_lop_Fan
+namespace XAY_DUNG_LOP_FAN
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Fan firstFan = new Fan();
+            Fan secondFan = new Fan();
+
+            firstFan.Speed = Fan.FAST;
+            firstFan.Radius = 10;
+            firstFan.Color = "yellow";
+            firstFan.On = true;
+
+            secondFan.Speed = Fan.MEDIUM;
+
+            Console.WriteLine(firstFan.DisplayFan());
+            Console.WriteLine(secondFan.DisplayFan());
         }
     }
 }
