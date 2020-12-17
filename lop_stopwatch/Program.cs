e2c7f9124f0b8a68254f054c45df5cd4ca6ebb02
unknown <leconghoangphuc@gmail.com>
2020-12-17 19:13:13 +0700

    fix

--- lop_stopwatch/Program.cs
+++ lop_stopwatch/Program.cs
@@ -6,7 +6,35 @@ namespace lop_stopwatch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = new int[100000];
+            Random arrRandom = new Random();
+            for( int i =0; i < arr.Length; i++)
+            {
+                arr[i] = arrRandom.Next(1,1001);
+            }
+            Stopwatch runStopwatch = new Stopwatch();
+            runStopwatch.Start();
+            sortArr(ref arr);
+            runStopwatch.Stop();
+            
+            Console.WriteLine($"{runStopwatch.GetElapsedTime()}");
+
+        }
+        static int[] sortArr(ref int[] array)
+        {
+            for(int i= 0; i < array.Length; i++)
+            {
+                int minIndex = i;
+                for(int j = i; j < array.Length; j++)
+                {
+                    if(array[minIndex] > array[j]) minIndex = array[j];
+
+                }
+                int temp = array[i];
+                array[i] = array[minIndex];
+                array[minIndex] = temp;
+            }
+            return array;  
         }
     }
 }
